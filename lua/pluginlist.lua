6ea56a22ea5e832ae05aef35dca0806c8421e9e2
Gabriel L. <gabriellovato15@gmail.com>
2024-01-27 12:11:46 -0300

    Organized plugins

--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -3,6 +3,7 @@ return {
 	"williamboman/mason-lspconfig.nvim",
 	"neovim/nvim-lspconfig",
 	"folke/neodev.nvim", -- nvim's namespace for lua lsp
+	"numToStr/Comment.nvim",
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x",
@@ -14,10 +15,10 @@ return {
 		},
 	},
 	{
-	  "ray-x/lsp_signature.nvim",
-	  event = "VeryLazy",
-	  opts = {},
-	  config = function(_, opts) require("lsp_signature").setup(opts) end
+		"ray-x/lsp_signature.nvim",
+		event = "VeryLazy",
+		opts = {},
+		config = function(_, opts) require("lsp_signature").setup(opts) end
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
@@ -48,28 +49,10 @@ return {
 		dependencies = {
 			"nvim-tree/nvim-web-devicons"
 		},
-		config = function()
-			require("lualine").setup({
-				icons_enabled = true,
-				theme = "catppuccin"
-			})
-		end
 	},
 	{
-		-- cool theme!
 		"catppuccin/nvim",
-
 		name="catppuccin",
 		priority = 1000,
-
-		config = function()
-			vim.cmd "colorscheme catppuccin"
-		end
-	},
-	{
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end
 	},
 }
